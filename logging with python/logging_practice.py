fe24ca152e41823e43c998fe528d4dfdc2197bac
Tejas Patil <tejas.patiltp2802@gmail.com>
2025-06-25 10:15:24 +0530

    daily practice

---
+++ logging with python/logging_practice.py
@@ -0,0 +1,12 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format={}
+                    )
+
+logging.info('this is info msg')
+logging.warning('this is warning msg')
+logging.error('this is error msg')
+logging.critical('this is the basic format')
+
+logging.
