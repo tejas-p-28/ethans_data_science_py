61d8b93d6905c01bc4a0c0c4d4b8fe7f44779948
Tejas Patil <tejas.patiltp2802@gmail.com>
2026-01-12 10:24:16 +0530

    daily practice

--- daily practice/.ipynb_checkpoints/practice.py
+++ daily practice/.ipynb_checkpoints/practice.py
@@ -1,57 +1,71 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        result = {}
-        for i, num in enumerate(nums):
-            need = target - num
-            if need in result:
-                return [result[need], i]
-            result[num] = i
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         result = {}
+#         for i, num in enumerate(nums):
+#             need = target - num
+#             if need in result:
+#                 return [result[need], i]
+#             result[num] = i
 
         
 
-# Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# # Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        dummy = ListNode(0)
-        curr = dummy
-        carry = 0
+# class Solution(object):
+#     def addTwoNumbers(self, l1, l2):
+#         dummy = ListNode(0)
+#         curr = dummy
+#         carry = 0
 
-        while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
+#         while l1 or l2 or carry:
+#             v1 = l1.val if l1 else 0
+#             v2 = l2.val if l2 else 0
 
-            total = v1 + v2 + carry
-            carry = total // 10
-            digit = total % 10
+#             total = v1 + v2 + carry
+#             carry = total // 10
+#             digit = total % 10
 
-            curr.next = ListNode(digit)
-            curr = curr.next
+#             curr.next = ListNode(digit)
+#             curr = curr.next
 
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
+#             if l1:
+#                 l1 = l1.next
+#             if l2:
+#                 l2 = l2.next
 
-        return dummy.next
+#         return dummy.next
 
 
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        seen = set()
-        left = 0
-        max_len = 0
+# class Solution(object):
+#     def lengthOfLongestSubstring(self, s):
+#         seen = set()
+#         left = 0
+#         max_len = 0
 
-        for right in range(len(s)):
-            while s[right] in seen:
-                seen.remove(s[left])
-                left += 1
+#         for right in range(len(s)):
+#             while s[right] in seen:
+#                 seen.remove(s[left])
+#                 left += 1
 
-            seen.add(s[right])
-            max_len = max(max_len, right - left + 1)
+#             seen.add(s[right])
+#             max_len = max(max_len, right - left + 1)
 
-        return max_len
+#         return max_len
+
+
+
+num = 2467
+
+def is_even(num):
+    quotient = num // 10
+    number = num - (quotient * 10)
+    if number in [0,2,4,6,8]:
+        return True
+    else:
+        return False
+
+print(is_even(num))
